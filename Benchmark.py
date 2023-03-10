9eb65194b417dfaaa82c2ae40e2b76faa0e9af93
Arn Baudzus <baudzus.arn@baua.bund.de>
2023-03-10 13:48:18 +0100

    Implemented the autoencoder. Code is mostly copied from a tutorial at curiously.com

--- Benchmark.py
+++ Benchmark.py
@@ -203,4 +203,44 @@ for epoch in range(1,n_epochs +1):
     model = model.train()
 
     train_losses = []
-    for seq_true in trainingSet
+    for seq_true in trainingSet:
+
+        optimizer.zero_grad()
+        
+        seq_true = seq_true.to(device) # I think we can do this faster if we transfer the dataset to the gpu first and than do the training
+        seq_pred = model(seq_true)
+    
+        loss = criterion(seq_pred, seq_true)
+
+        loss.backward()
+        optimizer.step()
+
+        train_losses.append(loss.item())
+
+    val_losses = []
+
+    model = model.eval()
+    
+    with torch.no_grad():
+        for seq_true in val_dataset:
+
+            seq_true = seq_true.to(device)
+            seq_pred = model(seq_true)
+
+            loss = criterion(seq_pred,seq_true)
+            val_losses.append(loss.item())
+
+    train_loss = np.mean(train_loss)
+    val_loss = np.mean(val_loss)
+
+    history["train"].append(train_loss)
+    history["val"].append(val_loss)
+
+    if val_loss < best_loss:
+        best_loss = val_loss
+        best_model_wts = copy.deepcopy(model.state_dict())
+
+    print(f"Epoch {epoch}: train loss {train_loss} val loss {val_loss}.")
+
+model.load_state_dict(best_model_wts)
+    
