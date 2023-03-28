a3ffc0cfec21053f8ad57e0e75e42e4bdb449d93
Arn Baudzus <baudzus.arn@baua.bund.de>
2023-03-28 13:36:28 +0200

    Reworked the hyperparameters of the feed forward AE to be more versitile from the outside.

--- AEModels/FeedForward.py
+++ AEModels/FeedForward.py
@@ -5,7 +5,9 @@ from BlockAndDatablock import block
 class Model(block,nn.Module): #Plain Feed Forward Encoder....
     
     def _getDefaultHPs(self):
-        return {"InputSize":150}
+        return {"InputSize":150,
+                "LayerSequence":[100,50,20,50,100],
+                "Activation Function":"ReLU"}
     
     def __init__(self,Dimensions,device,**HyperParameters):
         
@@ -15,21 +17,26 @@ class Model(block,nn.Module): #Plain Feed Forward Encoder....
         block.__init__(self,"FeedForwardAE",**HyperParameters) 
         nn.Module.__init__(self)
         
+        activations = {
+                    "ReLU":torch.nn.ReLU,
+                    "Sigmoid":torch.nn.Sigmoid,
+                    "tanh":torch.nn.Tanh
+                }
+        
+        LayerStack = [0]*(2*len(self.HP["LayerSequence"])+1)
+        
+        #Adding the input/output size to the front and end of the layer stack.
+        actualSequence = [Dimensions*self.HP["InputSize"]]+self.HP["LayerSequence"]+[Dimensions*self.HP["InputSize"]]
+        
+        for i in range(0,len(actualSequence)-2):
+            print(actualSequence[i],actualSequence[i+1])
+            LayerStack[2*i] = torch.nn.Linear(actualSequence[i],actualSequence[i+1])
+            LayerStack[2*i + 1] = activations[self.HP["Activation Function"]]()
+        
+        LayerStack[-1] = torch.nn.Linear(actualSequence[-2],actualSequence[-1])
 
         #Todo: Clean up and activationfunction as Hyperparameter
-        self.model = nn.Sequential(
-                    torch.nn.Linear(Dimensions*self.HP["InputSize"] , 100),
-                    torch.nn.ReLU(),
-                    torch.nn.Linear(100 , 50),
-                    torch.nn.ReLU(),
-                    torch.nn.Linear(50 , 20),
-                    torch.nn.ReLU(),
-                    torch.nn.Linear(20 , 50),
-                    torch.nn.ReLU(),
-                    torch.nn.Linear(50 , 100),
-                    torch.nn.ReLU(),    
-                    torch.nn.Linear(100,Dimensions*self.HP["InputSize"])
-                )
+        self.model = nn.Sequential(*LayerStack)
 
         self.model.to(device)
 
