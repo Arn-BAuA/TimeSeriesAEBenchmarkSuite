46cbbdbd802139f4b55082cc5b48ec144513f82e
Arn Baudzus <baudzus.arn@baua.bund.de>
2023-04-03 14:51:36 +0200

    Added wrapper class for the pytorch errors

---
+++ Errors/ReconstructionErrors.py
@@ -0,0 +1,27 @@
+import torch
+
+#A wrapper class that makes the default pytorch loss functions axessable in the Benchmark script.
+class TorchErrorWrapper():
+
+    def __init__(self,name,errorFunction,device):
+        self.name = name
+        self.device = device
+        self.errorFunction = errorFunction.to(device)
+
+    def __Name__(self):
+        return self.name
+
+    def calculate(self,model,Dataset):
+        Error = [0]*len(Dataset.Data())
+
+        for i in range(0,len(Dataset.Data())):
+            seq_true = Dataset.Data()[i]
+            seq_true = seq_true.to(self.device)
+            seq_pred = model(seq_true)
+
+            Error.append(self.errorFunction(seq_true,seq_pred).item())
+        
+        return np.mean(Error) 
+
+
+
