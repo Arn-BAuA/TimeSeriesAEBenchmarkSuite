864918a1f6acfacec30016cd97f0f7aa21becf6b
Arn Baudzus <baudzus.arn@baua.bund.de>
2023-05-04 15:08:58 +0200

    Batched training is working. (and boy does it run ..)

--- TestModel.py
+++ TestModel.py
@@ -21,8 +21,8 @@ Dimensions = 2 # Dataset dimensions
 #trainingSet,validationSet,testSet = Sines(Dimensions)
 trainingSet,validationSet,testSet = DataSet(Dimensions,DataSet = "UMD")
 
-model = FeedForwardAE(Dimensions,device,InputSize = trainingSet.Length())
-#model = LSTMAE(Dimensions,device,CellKind = "LSTM")
+#model = FeedForwardAE(Dimensions,device,InputSize = trainingSet.Length())
+model = LSTMAE(Dimensions,device,CellKind = "LSTM")
 #model = CNNAE(Dimensions,device)
 #model = CNNAE(Dimensions,device,hasFFTEncoder = True)
 
