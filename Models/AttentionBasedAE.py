b0984a8d4d27e0c5fbeee34d4b74e48906c1dbca
Arn Baudzus <baudzus.arn@baua.bund.de>
2023-05-08 16:27:54 +0200

    In theoiry, the transformer model is implemented and should work... lets see how this turns out.

--- Models/AttentionBasedAE.py
+++ Models/AttentionBasedAE.py
@@ -1,7 +1,8 @@
 import torch
+import numpy as np
 from torch import nn
 from BlockAndDatablock import block
-from Models.utility import strToActivation
+from Models.utility import strToActivation,createLinearNetwork
 
 class Model(block,nn.Module): #Using Vaswani Et al's Transformer as heart of an reconstruction based method...
     
@@ -14,8 +15,8 @@ class Model(block,nn.Module): #Using Vaswani Et al's Transformer as heart of an 
                 "nAttentionHeads":4,
                 "nTrEncoderLayers":2,
                 "nTrDecoderLayers":2,
-                "nTrFFDim":40,
-                "nPostprocessorLayers":2,
+                "TrFFDim":40,
+                "nPostprocessorLayers":4,
                 "ActivationFunctionPostprocessor":"ReLu",}
     
     def __init__(self,Dimensions,device,**HyperParameters):
@@ -27,11 +28,61 @@ class Model(block,nn.Module): #Using Vaswani Et al's Transformer as heart of an 
         nn.Module.__init__(self)
         
 
+        #Create Preprocessor
+        self.beginOfWords =  np.floor(np.linspace(0,self.HP["InputSize"]-self.HP["WordSize"],self.HP[nWords]))
+        self.endOfWords = self.beginOfWords+self.HP["WordSize"]
+
+        self.PreprocessorNetwork = createLinearNetwork(
+                                                        InputSize = self.HP["WordSize"]*Dimensions,
+                                                        OutputSize = self.HP["WordSize"],
+                                                        nLayers = self.HP["nPreprocessorLayers"],
+                                                        activationFunction = strToActivation(self.HP["ActivationFunctionPreprocessor"])() 
+                                                        )
         
+        
+        #Create Transformer
+        self.Transformer = nn.Transformer(
+                            d_model = self.HP["WordSize"],
+                            nhead = self.HP["nAttentionHeads"],
+                            num_encoder_layers = self.HP["nTrEncoderLayers"],
+                            num_decoder_layers = self.HP["nTrDecoderLayers"],
+                            dim_feedforward = self.HP["TrFFDim"],
+                            batch_first = True
+                            )
+
+        #Create Postprocessor
+         
+        self.PostprocessorNetwork = createLinearNetwork(
+                                                        InputSize = self.HP["WordSize"]*self.HP["nWords"],
+                                                        OutputSize = self.HP["InputSize"]*Dimensions,
+                                                        nLayers = self.HP["nPostprocessorLayers"],
+                                                        activationFunction = strToActivation(self.HP["ActivationFunctionPostprocessor"])() 
+                                                        )
+        self.PreprocessorNetwork.to(device)
+        self.Transformer.to(device)
+        self.PostprocessorNetwork.to(device)
 
     def forward(self,x):
         
+        #Preprocession
+        embeddedWindows = [0]*self.HP["nWords"]
 
-        return x
+        for i in range(0,self.HP["nWords"]):
+            window = x[:,:,self.beginOfWords[i],self.endOfWords[i]]
+            window = torch.flatten(window,start_dim=1)
+            embeddedWindows[i] = self.PreprocessorNetwork(window)
+        
+        #Transformer Stage
+        transformerEncoderInput = torch.stack(embeddedWindows,dim = 1)
+        transformerDecoderInput = torch.zeros_like(transformerEncoderInput)
+        
+        transformerOutput = self.Transformer(transformerEncoderInput,transformerDecoderInput)
+
+        #PostProcessing
+        postprocessorInput = torch.flatten(transformerOutput,start_dim=1)
+        output = self.PostprocessorNetwork(postprocessorInput)
+        output = torch.reshape(output,x.shape)
+
+        return output
 
 
