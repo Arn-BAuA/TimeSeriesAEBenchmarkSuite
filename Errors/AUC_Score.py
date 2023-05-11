3d39439322f13dc7d8576e70f42c5df604b5b8e2
Arn Baudzus <baudzus.arn@baua.bund.de>
2023-05-11 10:59:10 +0200

    AUC Score should work in theory.. . NOw that has to be implemented into the benchmark .py

--- Errors/AUC_Score.py
+++ Errors/AUC_Score.py
@@ -1,16 +1,73 @@
 import torch
 import numpy as np
+import copy
 
 class AUCScore:
 
     def __init__(self,device):
         self.device=device
 
-    def __scoreForClassificationData(self,TrueLabels,FalseLabels):
-        pass
+    def __scoreForClassificationData(self,TrueLabels,PredLabels):
+        TP = 0
+        FP = 0 
+        TN = 0
+        FN = 0
 
-    def __scoreForRegularData(self,TrueLables,FalseLabels):
-        pass
+        if 1 in TrueLables:
+            #By Construction of the DS: all 1, Anomaly
+            if 1 in PredLabels:
+                TP += 1  
+            else:
+                FN += 1
+        else:
+            #all 0, no anomaly
+            if 0 in PredLabels:
+                TN += 1
+            else:
+                FP += 1
+
+        return TP,FP,TN,FN
+
+    def __scoreForRegularData(self,TrueLables,PredLabels):
+        
+        TP = 0
+        FP = 0
+        TN = 0
+        FN = 0
+
+        acceptanceThreshhold = 0.5
+
+        intervals = []
+                          #Start, End, Label
+        currentInterval = [0,0,PredLables[0]]
+        
+        for i in range(1,PredLabels):
+            if not PredLabels[i] == currentInterval[2]:
+                currentInterval[1] = i-1
+                intervals.append(copy(currentInterval))
+                currentInterval = [i,i,PredLables[i]]
+
+        currentInterval[1] = len(PredLabels)-1
+        intervals.append(currentInterval)
+        
+        for interval in Intervals:
+            labelsRight = TrueLabels[interval[0]:interval[1]] == interval[2]
+            right = float(np.count_nonzero(labelsRight))/float(len(labelsRight))
+
+            if right > acceptanceThreshhold:
+                #Predicted the right thing... What is that thing?
+                if interval[2] == 1:
+                    TP += 1
+                else:
+                    TN += 1
+            else:
+                #Prdiction was wrong
+                if interval[2] == 1:
+                    FP += 1
+                else:
+                    FN += 1
+
+        return TP,FP,TN,FN
 
     def calculate(self,model,DataSet):
         
