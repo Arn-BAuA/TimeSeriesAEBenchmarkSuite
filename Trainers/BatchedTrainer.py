fc75069b68f87642ce188e20cf94f4ee3773b255
Arn Baudzus <baudzus.arn@baua.bund.de>
2023-05-26 10:46:31 +0200

    Merged all functionality of all trainers in the batched trainer. In theory, the communiacation with the gpu would be optimized too.

--- Trainers/BatchedTrainer.py
+++ Trainers/BatchedTrainer.py
@@ -14,8 +14,11 @@ class Trainer(block):
     
     def _getDefaultHPs(self):
         return {
-                "BatchSize":10,
-                "nBatches":100,
+                "TrainBatchSize":10,
+                "nTrainBatches":100,
+                "UseHistory":False,#If this is checked, after each epoch, the performance of the algorithm under test is measured on the validation set. If it supassed the best performance ever recorded in training, the model weights are saved. If it is worse than previous performance, the model weights are exchanged by the modle weights of the previous run. A little trick i picked up from this bolg: Time Series Anomaly Detection using LSTM Autoencoders with PyTorch in Python - https://curiousily.com/posts/time-series-anomaly-detection-using-lstm-autoencoder-with-pytorch-in-python/
+                "ValidationBatchSize":10, #Will be ignored if use history is false
+                "nValidationBatches":100, #Will be ignored if use history is false
                 }
 
     def __init__(self,model,device,**hyperParameters):
@@ -26,43 +29,71 @@ class Trainer(block):
         
         self.optimizer = torch.optim.Adam(model.parameters(),lr = 1e-3)
         self.criterion = nn.L1Loss(reduction = "sum").to(device)
-    
-    #Batching and Augmentation would be done here....
-    def setDataSets(self,trainingSet,validationSet):
-        data = trainingSet.Data()
-        Batches = [0] * self.HP["nBatches"]
-        
-        for i in range(0,self.HP["nBatches"]):
-            toConcatenate = [0]*self.HP["BatchSize"]
 
-            for j in range(0,self.HP["BatchSize"]):
+        if self.HPs["UseHistory"]:
+            self.best_loss = float('inf')
+    
+    def __prepareBatches(self,nBatches,BatchSize,dataSet):
+        
+        data = dataSet.Data()
+        Batches = [0] * nBatches
+        
+        for i in range(0,nBatches):
+            toConcatenate = [0]*BatchSize
+
+            for j in range(0,BatchSize):
                 
                 index=int(random()*len(data))
                 toConcatenate[j] = data[index]
 
             Batches[i] = torch.cat(toConcatenate)
+            Batches[i] = Batches[i].to(self.device)
+        return Batches
+
+    #Batching and Augmentation would be done here....
+    def setDataSets(self,trainingSet,validationSet):
+        self.trainBatches = self.__prepareBatches(self.HPs["nTrainBatches"],self.HPs["TrainBatchSize"],trainingSet)
+        self.validationBatches = self.__prepareBatches(self.HPs["nValidationBatches"],self.HPs["ValidationBatchSize"],validationSet)
         
-        print(Batches[0].size())
-        self.Batches = Batches
 
     def doEpoch(self,model):
     
         model = model.train()
-        for seq_true in self.Batches:
+        for seq_true in self.trainBatches:
 
             self.optimizer.zero_grad()
-        
-            seq_true = seq_true.to(self.device) # I think we can do this faster if we transfer the dataset to the gpu first and than do the training
+            
             seq_pred = model(seq_true)
-    
+            
             loss = self.criterion(seq_pred, seq_true)
 
             loss.backward()
             self.optimizer.step()
 
         model = model.eval()
+        
+        if self.HPs["UseHistory"]:
+            val_loss = []
     
+            with torch.no_grad():
+                for seq_true in self.validationBatches:
+
+                    seq_pred = model(seq_true)
+
+                    loss = self.criterion(seq_pred,seq_true)
+                    val_loss.append(loss.item())
+
+            val_loss = np.mean(val_loss)
+
+            if val_loss < self.best_loss:
+                self.best_loss = val_loss
+                self.best_model_wts = copy.deepcopy(model.state_dict())
+
+
         return model
 
     def finalizeTraining(self,model):
-        pass
+        if self.HPs["UseHistory"]:
+            if self.best_loss < float('inf'):
+                #this means, that the model has been trained before, so that the self.best_model_wts exists
+            model.load_state_dict(self.best_model_wts)
