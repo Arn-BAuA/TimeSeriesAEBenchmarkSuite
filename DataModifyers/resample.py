b118d5be32a331d63de937629e8b5f48d5613d4e
Arn Baudzus <baudzus.arn@baua.bund.de>
2024-01-22 15:51:54 +0100

    Created unit test for the resample method.

--- DataModifyers/resample.py
+++ DataModifyers/resample.py
@@ -6,10 +6,20 @@ import torch
 import numpy as np
 from DataModifyers.utility import applyToDataAndLabels
 
-def resample(newNrOfTimePoints,DataSet,mode="linear"):
+def resample(newNrOfTimePoints,DataSet):
     
     def interpolateData(DataPoint):
-        return torch.nn.functional.interpolate(DataPoint,size=newNrOfTimePoints,mode=mode)
+        
+        Data = DataPoint.numpy()
+        oldDataPositions = np.linspace(0,1,len(Data[0]))
+
+        InterpData = np.zeros([len(Data),newNrOfTimePoints])
+        newDataPositions = np.linspace(0,1,newNrOfTimePoints)
+
+        for dim in range(0,len(Data)):
+            InterpData[dim,:] = np.interp(newDataPositions,oldDataPositions,Data[dim])
+
+        return torch.Tensor(InterpData)
     
 
     # I am sure there is a better way, using a numpy build in function... What ever.
@@ -24,12 +34,13 @@ def resample(newNrOfTimePoints,DataSet,mode="linear"):
         
         for i in range(0,len(NewPositions)):
             
-            while (not oldPositionIndex == len(LabeledPositions)-2) and (NewPositions[i] > LabeledPositions[oldPositionIndex]):
+            while (not oldPositionIndex == len(LabeledPositions)-2) and (NewPositions[i] > LabeledPositions[oldPositionIndex+1]):
                 oldPositionIndex += 1
             
+
             distanceLeft = NewPositions[i]-LabeledPositions[oldPositionIndex]
-            distanceRight = LabeledPositions[oldPositionIndex+1]-NewPositions[i+1]
-            
+            distanceRight = LabeledPositions[oldPositionIndex+1]-NewPositions[i]
+
             if distanceRight >= distanceLeft:
                 newLabels[i] = LabelArray[oldPositionIndex]
             else:
--- UnitTests/TestSimpleDataModifiers.py
+++ UnitTests/TestSimpleDataModifiers.py
@@ -11,27 +11,36 @@ from DataModifyers.resample import resample
 class TestResample(unittest.TestCase):
 
     def test_linearFunction(self):
-        nBegin = 10
-        nInterp = 20
-
-        StartData = torch.Tensor(np.linspace(0,2,nBegin))
-        StartLabels = np.zeros(nBegin)
-        StartLabels[0:int(0.25*nBegin)] = 1
+        nBegins = [10,50,50]
+        nInterps = [100,100,10]
         
-        IntData = torch.Tensor(np.linspace(0,2,nInterp))
-        IntLabels = np.zeros(nBegin)
-        IntLabels[0:int(0.25*nInterp)] = 1
-    
-        DataSet = DataBlock("TestBlock",[StartData],1)
-        DataSet.setLabels(StartLabels)
+        
+        for i in range(0,len(nBegins)):
+            
+            nBegin = nBegins[i]
+            nInterp = nInterps[i]
 
-        InterpolatedSet = resample(nInterp,DataSet)
-        outData = InterpolatedSet.Data[0]
-        outLabels = InterpolatedSet.Labels[0]
+            StartData = torch.Tensor([np.linspace(0,2,nBegin)])
+            StartLabels = np.zeros(nBegin)
+            StartLabels[0:int(0.25*nBegin)] = 1
+            
+            IntData = torch.Tensor([np.linspace(0,2,nInterp)])
+            IntLabels = np.zeros(nInterp)
+            nOnesOriginal = np.sum(StartLabels == 1)
+            ratio = (nOnesOriginal-0.5)/(nBegin-1)
+            IntLabels[0:int(ratio*nInterp)] = 1
+        
+            DataSet = DataBlock("TestBlock",[StartData],1)
+            DataSet.setLabels([StartLabels])
 
-        self.assertEqual(outData,IntData)
-        self.assertEqual(outLabels,IntLabels)
+            InterpolatedSet = resample(nInterp,DataSet)
+            outData = InterpolatedSet.Data()[0]
+            outLabels = InterpolatedSet.Labels()[0]
 
+
+            self.assertEqual(torch.equal(torch.round(outData,decimals = 4),torch.round(IntData,decimals = 4)),True)
+            self.assertEqual(abs(np.sum(outLabels)-np.sum(IntLabels))<= 1,True)
+        
 
 if __name__ == "__main__":
     unittest.main()
