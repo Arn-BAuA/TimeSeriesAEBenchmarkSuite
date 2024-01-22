3b7f2ea6b6f0db1bc6de1f6f6e7f140117007274
Arn Baudzus <baudzus.arn@baua.bund.de>
2024-01-22 16:27:28 +0100

    Added Unit Test for normalisation

--- UnitTests/TestSimpleDataModifiers.py
+++ UnitTests/TestSimpleDataModifiers.py
@@ -6,6 +6,7 @@ import unittest
 import numpy as np
 import torch
 from BlockAndDatablock import DataBlock
+
 from DataModifyers.resample import resample
 
 class TestResample(unittest.TestCase):
@@ -41,6 +42,36 @@ class TestResample(unittest.TestCase):
             self.assertEqual(torch.equal(torch.round(outData,decimals = 4),torch.round(IntData,decimals = 4)),True)
             self.assertEqual(abs(np.sum(outLabels)-np.sum(IntLabels))<= 1,True)
         
+from DataModifyers.normalize import normalize
+
+class TestNormalisation(unittest.TestCase):
+
+    def test_normalisation(self):
+        sizeD1 =5
+        sizeD2 =5
+        nTensors = 10
+
+        data = [0]*nTensors
+
+        offset = 5
+        variation = 3
+
+        for i in range(0,nTensors):
+            data[i] = torch.rand(sizeD1,sizeD2)
+            data[i] = torch.mul(data[i],variation)
+            data[i] = torch.add(data[i],offset)
+
+        Set = DataBlock("TestData",data,sizeD1)
+
+        NormalizedSet = normalize(Set)
+
+        self.assertEqual(len(NormalizedSet.Data()),nTensors)
+        self.assertEqual(NormalizedSet.Data()[0].size()[0],sizeD1)
+        self.assertEqual(NormalizedSet.Data()[0].size()[1],sizeD2)
+        
+        self.assertEqual(abs(torch.mean(NormalizedSet.Data()[0]))<0.3,True)
+        self.assertEqual(abs(torch.std(NormalizedSet.Data()[0]))<=1.1,True)
+
 
 if __name__ == "__main__":
     unittest.main()
